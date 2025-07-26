78187557c23faac8e6a0181586be19c593ba9481
Tommalieh <muhammed@yourlea.io>
2025-07-26 14:11:13 +0300

    Rare Success 02

--- Program.cs
+++ Program.cs
@@ -1,39 +1,97 @@
+using System.Text;
+using DriveSmart.Application.Services;
+using DriveSmart.Persistence.Data;
+using DriveSmart.Persistence.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Define CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "https://www.drivia.it")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
+// Add services to the container.
+builder.Services.AddControllers();
+
+// SwashBuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+// Add JWT Auth
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var config = builder.Configuration;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = config["JwtSettings:Issuer"],
+            ValidAudience = config["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!))
+        };
+    });
+builder.Services.AddAuthorization();
 
-// Configure the HTTP request pipeline.
+// Register DB and Services
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<UserRefreshTokenRepository>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<TheoryService>();
+builder.Services.AddScoped<QuizService>();
 
-var summaries = new[]
+try
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var app = builder.Build();
+    app.UseExceptionHandler(errorApp =>
+    {
+        errorApp.Run(async context =>
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "application/json";
 
-app.MapGet("/weatherforecast", () =>
+            var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+            var exception = exceptionHandlerPathFeature?.Error;
+
+            var errorResponse = new
+            {
+                Message = "An unexpected error occurred.",
+                Detail = exception?.Message
+            };
+
+            await context.Response.WriteAsJsonAsync(errorResponse);
+        });
+    });
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseCors("AllowFrontend");
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception e)
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecast
-            (
-                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                Random.Shared.Next(-20, 55),
-                summaries[Random.Shared.Next(summaries.Length)]
-            ))
-        .ToArray();
-    return forecast;
-});
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine($".NET version: {Environment.Version}");
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("DriveSmart Exception: " + e);
+    Console.WriteLine("-------------------------------------------");
+    throw;
 }
